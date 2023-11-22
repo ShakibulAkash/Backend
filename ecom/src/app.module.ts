3436053beedcc257228a9807235d94ee52fea0b2
Kangkhita Urbee <86801356+KangkhitaUrbee@users.noreply.github.com>
2023-11-22 12:51:40 +0600

    Update app.module.ts

--- ecom/src/app.module.ts
+++ ecom/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
 import { UsersModule } from './users/users.module';
 import { SellerModule } from './Seller/seller.module';
+import { AdminModule } from './admin/admin.module';
 import { CurrentUserMiddleware } from './utility/middlewares/current-user.middleware';
 
 
